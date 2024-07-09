f64bbde409c33e5214f70b35ac9c974c52718f25
Kimd0ng <kdongh0406@gmail.com>
2024-07-09 23:43:10 +0900

    feat: community, myPage Route link 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/mainPage";
+import CommunityPage from "./pages/communityPage";
+import Header from "./components/Header/Header";
+import MyPage from "./pages/myPage";
 
 function App() {
   return (
     <BrowserRouter>
-      {/*헤더 들어갈 듯 */}
+        <Header/>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route path="/mypage" elemen={<MyPage/>}/>
       </Routes>
     </BrowserRouter>
   );
