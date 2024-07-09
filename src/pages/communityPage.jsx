e4b548cd13caac9bdac5d7d354cf628f093db2bf
Kimd0ng <kdongh0406@gmail.com>
2024-07-09 23:40:03 +0900

    feat: communityPage init

---
+++ src/pages/communityPage.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import SharePromptList from "../components/community/SharePromptList";
+
+
+function CommunityPage() {
+    return(
+        <>
+            <SharePromptList/>
+        </>
+    );
+}
+
+export default CommunityPage;
