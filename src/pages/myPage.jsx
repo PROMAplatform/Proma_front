006eb070f02b2a61ab7b6a426d51508a92edcb1b
Kimd0ng <kdongh0406@gmail.com>
2024-07-14 23:54:08 +0900

    feat: mypage state 별 component 연결

--- src/pages/myPage.jsx
+++ src/pages/myPage.jsx
@@ -1,9 +1,19 @@
-import React, {useState} from "react";
+import React from "react";
+import styles from "./myPage.module.css"
+import {useLocation} from "react-router-dom";
+import LikePromptList from "../components/community/mypage/LikePromptList";
+import WritePromptList from "../components/community/mypage/WritePromptList";
 
 function MyPage() {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const data = searchParams.get("data");
+
     return(
-        <>
-        </>
+        <div className={styles.backGround}>
+            {data === "like" && <LikePromptList />}
+            {data === "write" && <WritePromptList />}
+        </div>
     );
 }
 
