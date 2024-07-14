6015c6efc45110c7228559c2ce3ce0f481107e95
Kimd0ng <kdongh0406@gmail.com>
2024-07-14 23:55:37 +0900

    feat: header expandable button feat

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Header.module.css";
 import {Link} from "react-router-dom";
 import { ReactComponent as Logo2 } from "../../assets/images/comlogo2.svg";
 import { ReactComponent as Logo1 } from "../../assets/images/comlogo1.svg";
+import ExpandableButton from "./expandableButton/ExpandableButton";
 
 function Header(){
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+    };
+
     return(
         <div className={styles.headerContainer}>
             <div className={styles.logoContainer}>
@@ -15,10 +23,10 @@ function Header(){
                 <div>소개</div>
                 <div><Link to={"/community"}>커뮤니티</Link></div>
                 <div><Link to={"/"}>채팅</Link></div>
-                <div><Link to={"/mypage"}>로그인/마이페이지</Link></div>
+                <ExpandableButton buttonText="마이페이지"/>
             </div>
         </div>
-    )
+    );
 }
 
 export default Header;
