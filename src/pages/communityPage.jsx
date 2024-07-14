8b179d99578e56a0d8c70f114f40421cffe41286
Kimd0ng <kdongh0406@gmail.com>
2024-07-14 23:54:52 +0900

    feat: communitypage component 수정

--- src/pages/communityPage.jsx
+++ src/pages/communityPage.jsx
@@ -1,12 +1,15 @@
-import React, {useState} from "react";
-import SharePromptList from "../components/community/SharePromptList";
+import React from "react";
+import SharePromptList from "../components/community/Prompt/SharePromptList";
+import styles from "./communityPage.module.css";
+import FilterSection from "../components/community/FilterSection";
 
 
 function CommunityPage() {
     return(
-        <>
+        <div className={styles.backGround}>
+            <FilterSection/>
             <SharePromptList/>
-        </>
+        </div>
     );
 }
 
