cb1db105a8f8bfc87258e3264d7fc4c42bfdb8a5
kyujenius <hkj020607@gmail.com>
2024-07-24 14:26:37 +0900

    🐛 fix: 사이드 바 및 URL 일부 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import PromptMakingPage from "./pages/promptMakingPage";
 import MainPage from "./pages/mainPage";
 import CommunityPage from "./pages/communityPage";
 import Header from "./components/Header/Header";
 import MyPage from "./pages/myPage";
-import LikePromptList from "./components/community/mypage/LikePromptList";
+import KakaoAfterLogin from "./components/Login/KakaoLoginAfter";
 
 function App() {
   return (
     <BrowserRouter>
-        <Header/>
+      <Header />
       <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/community" element={<CommunityPage />} />
-          <Route path="/mypage" element={<MyPage/>}/>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/community" element={<CommunityPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route
+          path="/api/v1/auth/customers/kakao"
+          element={<KakaoAfterLogin />}
+        />
+        <Route path="/promptMaking" element={<PromptMakingPage />} />
       </Routes>
     </BrowserRouter>
   );
