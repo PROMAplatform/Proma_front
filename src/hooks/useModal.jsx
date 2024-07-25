6031faf75418f687d2e5a301e5f73d8002a5ddba
Kimd0ng <kdongh0406@gmail.com>
2024-07-25 17:47:20 +0900

    :sparkles: feat: 중복 컴포넌트 분리 및 hook feat

---
+++ src/hooks/useModal.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useCallback, useState } from 'react';
+
+function useModal(isOpen, onClose) {
+    const [isModalOpen, setIsModalOpen] = useState(isOpen);
+
+    const handleOverlayClick = useCallback(
+        (e) => {
+            if (e.target === e.currentTarget) {
+                onClose();
+            }
+        },
+        [onClose]
+    );
+
+    useEffect(() => {
+        setIsModalOpen(isOpen);
+    }, [isOpen]);
+
+    return { isModalOpen, handleOverlayClick };
+}
+
+export default useModal;
