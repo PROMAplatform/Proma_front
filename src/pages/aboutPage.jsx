65b092342c18605a6828cdd9f40cdb68bc70b6c0
kyujenius <hkj020607@gmail.com>
2024-08-08 22:20:19 +0900

    ✨ feat: 소개 페이지 에니메이션 추가 및 적용

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -3,26 +3,49 @@ import { useNavigate } from "react-router-dom";
 import { H1, B1 } from "../styles/font-styles";
 import NavigateButton from "../components/common/NavigateButton";
 import styles from "./aboutPage.module.css";
-import BlockAnimation from "../components/common/BlockAnimation";
+
+import promaAnimation from "../assets/animation/promaAnimation.json";
+import Lottie from "react-lottie";
 
 function AboutPage() {
   const navigate = useNavigate();
   const moveTOCommunity = () => {
     navigate("/community");
-  }
+  };
 
   const moveToChatting = () => {
     navigate("/");
-  }
+  };
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: promaAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   return (
     <div className={styles.container}>
-      <BlockAnimation />
-      <H1>모두를 위한 프롬프트 엔지니어링</H1>
-      <B1>블록형 프롬프트로 쉽고 빠르게 AI와 소통하세요</B1>
-      <div className={styles.buttonContainer}>
-        <NavigateButton title="community" variant="primary" onClick={moveTOCommunity}/>
-        <NavigateButton title="chatting" variant="secondary" onClick={moveToChatting}/>
+      <div className={styles.animationContainer}>
+        <Lottie options={defaultOptions} width={700} height={600} />
+      </div>
+      <div className={styles.contentContainer}>
+        <H1>모두를 위한 프롬프트 엔지니어링</H1>
+        <B1>블록형 프롬프트로 쉽고 빠르게 AI와 소통하세요</B1>
+        <div className={styles.buttonContainer}>
+          <NavigateButton
+            title="community"
+            variant="primary"
+            onClick={moveTOCommunity}
+          />
+          <NavigateButton
+            title="chatting"
+            variant="secondary"
+            onClick={moveToChatting}
+          />
+        </div>
       </div>
     </div>
   );
