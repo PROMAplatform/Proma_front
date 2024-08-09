a3f8b6e35af6879cb3ff32a57b81a4b6b41ff0b2
Kimd0ng <kdongh0406@gmail.com>
2024-08-09 18:02:28 +0900

    :lipstick: style: modal css 개선

--- src/components/SharingPrompt/modal/DetailModal/components/TopSection.jsx
+++ src/components/SharingPrompt/modal/DetailModal/components/TopSection.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import styles from "./TopSection.module.css";
-import { ReactComponent as LikeImg } from "../../../../../assets/images/likeImg.svg";
-import { ReactComponent as LikeTrue } from "../../../../../assets/images/heartTrue.svg";
-import { useCommunityHooks } from "../../../../../api/community/community";
-import { useSetRecoilState } from "recoil";
-import { stateChange } from "../../../../../recoil/community/communityRecoilState";
+import {ReactComponent as LikeImg} from "../../../../../assets/images/likeImg.svg";
+import {ReactComponent as LikeTrue} from "../../../../../assets/images/heartTrue.svg";
+import {ReactComponent as ExitIcon} from "../../../../../assets/images/exitIcon.svg";
+import {useCommunityHooks} from "../../../../../api/community/community";
+import {useSetRecoilState} from "recoil";
+import {stateChange} from "../../../../../recoil/community/communityRecoilState";
+import {methodImage} from "../../../util/methodImage";
+import {B4, B6, H3} from "../../../../../styles/font-styles";
 
 function TopSection({ post, onClose }) {
     const setStateChange = useSetRecoilState(stateChange);
@@ -23,28 +26,40 @@ function TopSection({ post, onClose }) {
             setLocalLikeCount((prevValue) => prevValue + 1);
         }
 
-        setStateChange((prevValue) => prevValue + 1);
         console.log("좋아요 버튼 : ", post.postId);
+    }
+
+    function handleCloseClick() {
+        setStateChange(prevValue => prevValue + 1);
+        onClose();
     }
 
     return (
         <div>
             <div className={styles.topSection}>
-                <div>{post.promptMethod}</div>
+                <div className={styles.methodSection}>
+                    <img src={methodImage[post.promptMethod]} alt={post.promptMethod}/>
+                    {post.promptMethod}
+                </div>
                 <div className={styles.likeSection}>
-                    <div>{post.postTitle}</div>
-                    <div>by {post.userName}</div>
-                    <div className={styles.likeButton}>
-                        {localLikeCount}
-                        {localLikeState === true ? (
-                            <LikeTrue onClick={handleClickLike} />
-                        ) : (
-                            <LikeImg onClick={handleClickLike} />
-                        )}
+                    <div>
+                        <H3>{post.postTitle}</H3>
+                    </div>
+                    <div>
+                        <B4>by {post.userName}</B4>
+                    </div>
+                    <div className={styles.likeButtonSection}>
+                        <div className={styles.likeButton}>
+                            {localLikeState === true ? <LikeTrue onClick={handleClickLike}/>
+                                : <LikeImg onClick={handleClickLike}/>}
+                            <B6>{localLikeCount}</B6>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <button onClick={onClose}>닫기</button>
+                <div className={styles.closeSection}>
+                    <div className={styles.closeButton}>
+                        <ExitIcon onClick={handleCloseClick}/>
+                    </div>
                 </div>
             </div>
         </div>
