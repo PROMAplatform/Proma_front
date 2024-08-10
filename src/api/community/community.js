bd98338a2be258856229ddd4a1844cfc2bb7acf4
Kimd0ng <kdongh0406@gmail.com>
2024-08-10 19:30:29 +0900

    :bug: fix: interceptor 수정

--- src/api/community/community.js
+++ src/api/community/community.js
@@ -17,7 +17,6 @@ export const useCommunityHooks = () => {
     const setMakePromptDetail = useSetRecoilState(makePromptDetailState);
     const setCommunityPromptListPage = useSetRecoilState(communityPromptListPageState);
     const setIsLoadingcommunity = useSetRecoilState(isLoadingCommunityState);
-    const auth = localStorage.getItem("accessToken");
 
     //공유된 프롬프트 리스트 조회
     const getCommunityPromptList = async (
@@ -26,48 +25,47 @@ export const useCommunityHooks = () => {
         searchQuery,
         currentPage,
     ) => {
-        if (auth) {
-            setIsLoadingcommunity(true);
-            try {
-                const params = {
-                    userId: 1,
-                    category: selectedCategory,
-                    search: searchQuery,
-                    page: currentPage,
-                    size: 9,
-                };
+        applyInterceptors(communityIntstance);
+        setIsLoadingcommunity(true);
+        try {
+            const params = {
+                userId: 1,
+                category: selectedCategory,
+                search: searchQuery,
+                page: currentPage,
+                size: 9,
+            };
 
-                // sortOrder에 따라 정렬 기준 추가
-                if (sortOrder === "latest") {
-                    // params.latest = "desc";
-                    params.like = "";
-                } else if (sortOrder === "like") {
-                    params.like = "desc";
-                    //params.latest = "";
-                }
-
-                const response = await sendRequest(communityIntstance, "get", "", {
-                    params,
-                });
-
-                const {responseDto} = response.data;
-                const {selectPrompt, pageInfo} = responseDto;
-
-                setCommunityPromptList(selectPrompt);
-                setCommunityPromptListPage({
-                    currentPage: pageInfo.currentPage || 0,
-                    totalPages: pageInfo.totalPages || 0,
-                    pageSize: 9,
-                    totalItems: pageInfo.totalItems || 0,
-                    currentItems: pageInfo.currentItems,
-                });
-            } catch (error) {
-                setCommunityPromptList([]);
-                setCommunityPromptListPage(null);
-                console.error("Error:", error);
-            } finally {
-                setIsLoadingcommunity(false);
+            // sortOrder에 따라 정렬 기준 추가
+            if (sortOrder === "latest") {
+                // params.latest = "desc";
+                params.like = "";
+            } else if (sortOrder === "like") {
+                params.like = "desc";
+                //params.latest = "";
             }
+
+            const response = await sendRequest(communityIntstance, "get", "", {
+                params,
+            });
+
+            const {responseDto} = response.data;
+            const {selectPrompt, pageInfo} = responseDto;
+
+            setCommunityPromptList(selectPrompt);
+            setCommunityPromptListPage({
+                currentPage: pageInfo.currentPage || 0,
+                totalPages: pageInfo.totalPages || 0,
+                pageSize: 9,
+                totalItems: pageInfo.totalItems || 0,
+                currentItems: pageInfo.currentItems,
+            });
+        } catch (error) {
+            setCommunityPromptList([]);
+            setCommunityPromptListPage(null);
+            console.error("Error:", error);
+        } finally {
+            setIsLoadingcommunity(false);
         }
     };
 
@@ -141,139 +139,125 @@ export const useCommunityHooks = () => {
 
     //공유된 프롬프트 스크랩
     const scrapPrompt = async (postId) => {
-        if (auth){
-            setIsLoadingcommunity(true);
-            try {
-                const params = {
-                    userId: 1,
-                };
-                const url = createUrl(`/scrap/${postId}`, params);
-                const response = await sendRequest(communityIntstance, "post", url);
+        applyInterceptors(communityIntstance);
+        setIsLoadingcommunity(true);
+        try {
+            const params = {
+                userId: 1,
+            };
+            const url = createUrl(`/scrap/${postId}`, params);
+            const response = await sendRequest(communityIntstance, "post", url);
 
-                return response.data.responseDto;
-            } catch (error) {
-                console.error("error : ", error);
-            } finally {
-                setIsLoadingcommunity(false);
-            }
-        } else {
-            applyInterceptors(communityIntstance);
+            return response.data.responseDto;
+        } catch (error) {
+            console.error("error : ", error);
+        } finally {
+            setIsLoadingcommunity(false);
         }
     };
 
     //공유된 프롬프트 좋아요 버튼
     const likePost = async (postId) => {
-        if (auth) {
-            setIsLoadingcommunity(true);
-            try {
-                const params = {
-                    userId: 1,
-                };
-                const url = createUrl(`/like/${postId}`, params);
 
-                const response = await sendRequest(communityIntstance, "post", url);
+        applyInterceptors(communityIntstance);
+        setIsLoadingcommunity(true);
+        try {
+            const params = {
+                userId: 1,
+            };
+            const url = createUrl(`/like/${postId}`, params);
 
-                return response.data.responseDto;
-            } catch (error) {
-                console.error("error : ", error);
-            } finally {
-                setIsLoadingcommunity(false);
-            }
-        } else {
-            applyInterceptors(communityIntstance);
+            const response = await sendRequest(communityIntstance, "post", url);
+
+            return response.data.responseDto;
+        } catch (error) {
+            console.error("error : ", error);
+        } finally {
+            setIsLoadingcommunity(false);
         }
-
     };
 
     //내가 작성한 프롬프트 리스트 id, title
     const getMakePromptList = async () => {
-        if (auth) {
-            setIsLoadingcommunity(true);
-            try {
-                const response = await sendRequest(
-                    communityIntstance,
-                    "get",
-                    "/titleList",
-                    {
-                        params: {
-                            userId: 1,
-                        },
-                    },
-                );
 
-                setMakePromptList(response.data.responseDto.promptList);
-            } catch (error) {
-                console.error("error : ", error);
-            } finally {
-                setIsLoadingcommunity(false);
-            }
-        } else {
-            applyInterceptors(communityIntstance);
+        applyInterceptors(communityIntstance);
+        setIsLoadingcommunity(true);
+        try {
+            const response = await sendRequest(
+                communityIntstance,
+                "get",
+                "/titleList",
+                {
+                    params: {
+                        userId: 1,
+                    },
+                },
+            );
+
+            setMakePromptList(response.data.responseDto.promptList);
+        } catch (error) {
+            console.error("error : ", error);
+        } finally {
+            setIsLoadingcommunity(false);
         }
     };
 
     //내 프롬프트 상세조회
     const getPromptDetail = async (promptId) => {
-        if (auth) {
-            setIsLoadingcommunity(true);
-            try {
-                const response = await sendRequest(
-                    communityIntstance,
-                    "get",
-                    `/detail/${promptId}`,
-                    {
-                        params: {
-                            userId: 1,
-                        }
-                    });
-                setMakePromptDetail({
-                    pomptId: response.data.responseDto.promptId,
-                    promptMethod: response.data.responseDto.promptMethod,
-                    promptTitle: response.data.responseDto.promptTitle,
-                    promptCategory: response.data.responseDto.promptCategory,
-                    promptDescription: response.data.responseDto.promptDescription,
-                    promptPreview: response.data.responseDto.promptPreview,
-                    listPromptAtom: response.data.responseDto.listPromptAtom,
+        applyInterceptors(communityIntstance);
+        setIsLoadingcommunity(true);
+        try {
+            const response = await sendRequest(
+                communityIntstance,
+                "get",
+                `/detail/${promptId}`,
+                {
+                    params: {
+                        userId: 1,
+                    }
                 });
-            } catch (error) {
-                console.error("Error:", error);
-            } finally {
-                setIsLoadingcommunity(false);
-            }
-        } else {
-            applyInterceptors(communityIntstance);
+            setMakePromptDetail({
+                pomptId: response.data.responseDto.promptId,
+                promptMethod: response.data.responseDto.promptMethod,
+                promptTitle: response.data.responseDto.promptTitle,
+                promptCategory: response.data.responseDto.promptCategory,
+                promptDescription: response.data.responseDto.promptDescription,
+                promptPreview: response.data.responseDto.promptPreview,
+                listPromptAtom: response.data.responseDto.listPromptAtom,
+            });
+        } catch (error) {
+            console.error("Error:", error);
+        } finally {
+            setIsLoadingcommunity(false);
         }
     };
 
     //프롬프트 공유
     const sharePrompt = async (promptId, data) => {
-        if (auth) {
-            setIsLoadingcommunity(true);
-            try {
-                const params = {
-                    userId: 1,
-                };
+        applyInterceptors(communityIntstance);
+        setIsLoadingcommunity(true);
+        try {
+            const params = {
+                userId: 1,
+            };
 
-                const url = createUrl(`/distribute/${promptId}`, params);
+            const url = createUrl(`/distribute/${promptId}`, params);
 
-                const response = await sendRequest(
-                    communityIntstance,
-                    "post",
-                    url,
-                    {
-                        postTitle: data.title,
-                        postDescription: data.description,
-                        postCategory: data.category,
-                    },
-                );
-                return response.data.responseDto;
-            } catch (error) {
-                console.log("error : ", error);
-            } finally {
-                setIsLoadingcommunity(false);
-            }
-        } else {
-            applyInterceptors(communityIntstance);
+            const response = await sendRequest(
+                communityIntstance,
+                "post",
+                url,
+                {
+                    postTitle: data.title,
+                    postDescription: data.description,
+                    postCategory: data.category,
+                },
+            );
+            return response.data.responseDto;
+        } catch (error) {
+            console.log("error : ", error);
+        } finally {
+            setIsLoadingcommunity(false);
         }
     };
 
