abc8bba28c57c6dcdd4e6a5dc5f09ece9ef791af
jungsun <dlwjdtjs0111@gmail.com>
2024-09-05 21:10:57 +0900

    💄 style: 프롬프트 카테고리 스크롤 버튼 추가 및 에러 수정 (#54)

--- src/components/SideBar/components/Prompt/PromptList.jsx
+++ src/components/SideBar/components/Prompt/PromptList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import PromptListItem from "./PromptListItem";
 import styles from "./PromptList.module.css";
 import { useRecoilValue } from "recoil";
@@ -6,10 +6,14 @@ import { promptListState } from "../../../../recoil/prompt/promptRecoilState";
 import { useChattingRoomHooks } from "../../../../api/chatting/chatting";
 import SkeletonListItem from "../SkeletonListItem";
 import blockIcon from "../../../../assets/images/blockIcon.svg";
+import rightScrollIcon from "../../../../assets/images/rightScrollIcon.svg";
+import leftScrollIcon from "../../../../assets/images/leftScrollIcon.svg";
 import { B4 } from "../../../../styles/font-styles";
 import { t } from "i18next";
 
 function PromptList() {
+    const scrollRef = useRef(null);
+    const [scrollState, setScrollState] = useState("right");
     const [isLoading, setIsLoading] = useState(true);
     const { fetchPromptList } = useChattingRoomHooks();
 
@@ -47,20 +51,64 @@ function PromptList() {
                   proma.promptCategory.includes(selectedCategory),
               );
 
+    useEffect(() => {
+        const handleScroll = (event) => {
+            const scrollContainer = scrollRef.current;
+            const {scrollLeft, scrollWidth, clientWidth} = scrollContainer;
+
+            if (scrollContainer) {
+                if (scrollLeft <= (scrollWidth - clientWidth) / 2) {
+                    setScrollState("right");
+                } else {
+                    setScrollState("left");
+                }
+            }
+        }
+        const scrollContainer = scrollRef.current;
+        scrollContainer.addEventListener('scroll', handleScroll);
+
+        handleScroll();
+         // 컴포넌트가 unmount될 때 이벤트 리스너 제거
+        return () => {
+            scrollContainer.removeEventListener('scroll', handleScroll);
+        };
+    }, [])
+
+    const handleLeftScroll = () => {
+        const scrollContainer = scrollRef.current;
+        if (scrollContainer) {
+            scrollContainer.scrollLeft = 0;
+        }
+    }
+
+    const handleRightScroll = () => {
+        const scrollContainer = scrollRef.current;
+        if (scrollContainer) {
+            scrollContainer.scrollLeft = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+        }
+    }
     return (
         <div className={styles.container}>
             <div className={styles.categoryContainer}>
-                {allCategories.map((category) => (
-                    <button
-                        key={category}
-                        className={`${styles.categoryButton} ${
-                            selectedCategory === category ? styles.active : ""
-                        }`}
-                        onClick={() => setSelectedCategory(category)}
-                    >
-                        {category}
-                    </button>
-                ))}
+                <div className={styles.categoryScrollContainer} ref={scrollRef}>
+                    {allCategories.map((category) => (
+                        <button
+                            key={category}
+                            className={`${styles.categoryButton} ${
+                                selectedCategory === category ? styles.active : ""
+                            }`}
+                            onClick={() => setSelectedCategory(category)}
+                        >
+                            {category}
+                        </button>
+                    ))}
+                </div>
+                {scrollState !== "left" && (
+                    <img src={rightScrollIcon} alt="right scroll" className={styles.rightScrollIcon} onClick={handleRightScroll}/>
+                )}
+                {scrollState !== "right" && (
+                    <img src={leftScrollIcon} alt="left scroll" className={styles.leftScrollIcon} onClick={handleLeftScroll}/>
+                )}
             </div>
             <div className={styles.promptListWrapper} data-tour="promptList">
                 {isLoading ? ( // 로딩 중일 때
@@ -73,9 +121,9 @@ function PromptList() {
                     </div>
                 ) : filteredPrompts.length > 0 ? (
                     <div className={styles.promptListContainer}>
-                        {filteredPrompts.slice().map((prompt, index) => (
+                        {filteredPrompts.slice().map((prompt) => (
                             <PromptListItem
-                                key={index}
+                                key={prompt.promptId}
                                 emoji={prompt.emoji}
                                 promptId={prompt.promptId}
                                 name={prompt.promptTitle}
